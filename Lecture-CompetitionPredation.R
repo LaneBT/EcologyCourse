60b9da80770aefb7f79a7c38f6384a36acab9c3f
Laney Tellegen <laneytellegen@Karens-MacBook-Pro-2.local>
2025-03-12 21:32:15 -0500

    Notes from Ecology Lecture

---
+++ Lecture-CompetitionPredation.R
@@ -0,0 +1,16 @@
+#R script for Competition and Predation activity
+
+#### Question 2
+
+#Prey model
+r<-0.25
+K<-2000
+a<-0.01 #capture efficiency
+
+#Predator model
+b<-0.008 #conversion efficiency
+m<-0.2 #mortality
+
+#starting population sizes
+N1<-1000
+P1<-20
